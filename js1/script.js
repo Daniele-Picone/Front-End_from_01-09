da85beea78ad3a0019691d6b6d5a406005b0bcc2
Daniele Picone <danielepicone97@gmail.com>
2025-09-08 15:01:04 +0200

    esercizi_js

---
+++ js1/script.js
@@ -0,0 +1,64 @@
+
+// variabili
+
+let numero = 42;
+let parola= 'ciao';
+let flag = false;
+let nulla = null
+let nonDefinito 
+
+console.log('il numero è :' +" "+  + numero , 'la parola è:'+' '+ parola , 'tutto quest è:'+ '' +' ' + flag , 'ma anche:' + ' ' + nulla +'e'+ nonDefinito );
+//  operatori
+let somma = 7 + 5;
+console.log('la somma è' + ' '+ somma);
+
+let sottraizone = 10 - 3 ;
+console.log('la sottrazione è' + ' '+ sottraizone);
+
+let prodotto = 4 * 6;
+
+console.log('il prodotto è' + ' '+ prodotto);
+
+let quozionte = 20 / 4; 
+console.log('il quoziente è' + ' '+ quozionte);
+
+let resto = 17 % 3 ;
+console.log('il resto è' + ' '+ resto);
+
+let punti =50 
+punti += 10;
+console.log('la somma è' + ' '+  punti);
+
+
+
+console.log(somma == '12');
+console.log(somma === '12');
+
+console.log(resto != 2);
+console.log(resto !== '1');
+
+// condizioni
+
+let eta = 20 ;
+
+if( eta < 18){
+    console.log('sei minorenne');
+    
+}
+
+let voto = 30;
+
+if(voto === 30){
+    console.log('ottimo');
+    
+}else if(voto >= 18 ){
+    console.log('buono');
+    
+}else{
+    console.log('insuficiente');
+    
+}
+
+
+
+
